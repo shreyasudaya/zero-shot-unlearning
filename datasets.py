507d8b00253a4fbe60f46dd1e08e6ec9fe391334
Shreyas Udaya <94735680+shreyasudaya@users.noreply.github.com>
2025-01-01 17:49:54 +0530

    cifar100 suppor

--- datasets.py
+++ datasets.py
@@ -5,6 +5,28 @@ import os
 from torchvision.datasets import ImageFolder
 from torchvision.datasets.utils import download_url
 
+def cifar100(root = './'):
+    transform = tt.Compose([
+        tt.ToTensor(),
+        tt.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    dataset_url = "https://s3.amazonaws.com/fast-ai-imageclas/cifar100.tgz"
+    download_url(dataset_url, '.')
+
+    # Extract from archive
+    with tarfile.open('./cifar100.tgz', 'r:gz') as tar:
+        tar.extractall(path='./data')
+
+    # Look into the data directory
+    data_dir = os.path.join(root, 'data/cifar100')
+    #print(os.listdir(data_dir))
+    #classes = os.listdir(data_dir + "/train")
+    
+    #train_ds = torchvision.datasets.CIFAR100(root='./', train=True, download=True, transform=transform)
+    #valid_ds = torchvision.datasets.CIFAR100(root='./', train=False, download=True, transform=transform)
+    train_ds = ImageFolder(data_dir+'/train', transform)
+    valid_ds = ImageFolder(data_dir+'/test', transform)
+    return train_ds, valid_ds
 
 def cifar10(root = './'):
     transform = tt.Compose([
